f00dfb7efb03078eb524e1097543b3d16bed1cce
zhuangchongyi <1909017815@qq.com>
2020-11-14 16:29:02 +0800

    准备接入商城

---
+++ src/main/java/com/dc/project/open/controller/AuthController.java
@@ -0,0 +1,12 @@
+package com.dc.project.open.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author zhuangcy
+ * @date 2020/11/14 16:26
+ * @description 第三方登录接口
+ */
+@RestController
+public class AuthController {
+}
